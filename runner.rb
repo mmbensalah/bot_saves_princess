3ba9e6be2f72669f8671a191339a9694cc113b94
mmbensalah <mmbensalah@gmail.com>
2019-11-23 21:31:53 -0700

    Removed pry

--- runner.rb
+++ runner.rb
@@ -1,7 +1,8 @@
 require_relative 'save_princess'
 
+puts "Enter an odd number to create matrix i.e. 3"
 input_size = gets.chomp.to_i
+puts "Enter the board layout placing 'm' in the center i.e. per the HackerRank instructions"
 start_game = SavePrincess.new(input_size)
-binding.pry
 start_game.build_matrix
 start_game.display_path_to_princess
