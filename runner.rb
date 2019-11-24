c3251fc41502ac34a0c1c59aa8b712b15333e5a7
mmbensalah <mmbensalah@gmail.com>
2019-11-23 21:29:52 -0700

    Added clarifying instructions

--- runner.rb
+++ runner.rb
@@ -1,6 +1,8 @@
 require_relative 'save_princess'
 
+puts "Input an odd number for the matrix size i.e. 3"
 input_size = gets.chomp.to_i
+puts "Input board layout i.e. as outlined in HackerRank instructions"
 start_game = SavePrincess.new(input_size)
 binding.pry
 start_game.build_matrix
