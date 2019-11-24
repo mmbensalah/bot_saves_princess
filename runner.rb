480715e7f755289e2cb868b67bcf5a3ecd9560a5
mmbensalah <mmbensalah@gmail.com>
2019-11-23 21:23:32 -0700

    Add additional test/functionality to calculate output for input board

--- runner.rb
+++ runner.rb
@@ -2,4 +2,6 @@ require_relative 'save_princess'
 
 input_size = gets.chomp.to_i
 start_game = SavePrincess.new(input_size)
+binding.pry
 start_game.build_matrix
+start_game.display_path_to_princess
--- spec/save_princess_spec.rb
+++ spec/save_princess_spec.rb
@@ -1,13 +1,41 @@
 require 'rspec'
+require 'stringio'
 require_relative '../save_princess'
 
 RSpec.describe SavePrincess do
-  context 'builds matrix' do
+  describe '#build_matrix' do
     it 'creates matrix corresponding to input size' do
       input_size = 3
       save_princess = SavePrincess.new(input_size)
-      matrix = save_princess.build_matrix
-      expect(matrix.class).to eq Array
+      expect(save_princess.build_matrix.count).to eq(3)
+      expect(save_princess.build_matrix.class).to eq(Array)
+    end
+    it 'creates correct path when princess in 0,0 position' do
+      input_size = 3
+      save_princess = SavePrincess.new(input_size)
+      save_princess.stub(:completed_matrix) { [["p","-","-"],["-","m","-"],["-","-","-"]] }
+      expect(save_princess.display_path_to_princess).to include("LEFT" "UP")
+    end
+
+    it 'creates correct path when princess in 0,2 position' do
+      input_size = 3
+      save_princess = SavePrincess.new(input_size)
+      save_princess.stub(:completed_matrix) { [["-","-","-"],["-","m","-"],["P","-","-"]] }
+      expect(save_princess.display_path_to_princess).to include("LEFT" "DOWN")
+    end
+
+    it 'creates correct path when princess in 2,0 position' do
+      input_size = 3
+      save_princess = SavePrincess.new(input_size)
+      save_princess.stub(:completed_matrix) { [["-","-","p"],["-","m","-"],["-","-","-"]] }
+      expect(save_princess.display_path_to_princess).to include("RIGHT" "UP")
+    end
+
+    it 'creates correct path when princess in 2,2 position' do
+      input_size = 3
+      save_princess = SavePrincess.new(input_size)
+      save_princess.stub(:completed_matrix) { [["-","-","-"],["-","m","-"],["-","-","p"]] }
+      expect(save_princess.display_path_to_princess).to include("RIGHT" "DOWN")
     end
   end
 end
