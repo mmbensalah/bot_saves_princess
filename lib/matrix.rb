83f60ff4180bdd8441cc4289c2328d806aafa3d1
mmbensalah <mmbensalah@gmail.com>
2019-11-25 13:57:49 -0700

    Original brute force code

---
+++ lib/matrix.rb
@@ -0,0 +1,40 @@
+class Matrix
+  attr_reader :size, :matrix, :princess, :bot
+
+  def initialize(size)
+    @size = size
+  end
+
+  def build_matrix
+    matrix = []
+    @size.times do
+      matrix << get_input_matrix
+    end
+    @matrix = matrix.map{|row| row.chars}
+  end
+
+  def find_bot_position
+    x = @matrix.count/2
+    y = @matrix.count/2
+    @bot = Bot.new([y, x])
+  end
+
+  def find_princess_position
+    @matrix.each_with_index do |row, x|
+      row.each_with_index do |position, y|
+        @princess = Princess.new([y, x]) if position == "p"
+      end
+    end
+    @princess
+  end
+
+  private
+
+  def get_input_matrix
+    input = $stdin.gets.chomp
+    if input.length != @size
+      raise ArgumentError.new("Incorrect number of columns")
+    end
+    input
+  end
+end
